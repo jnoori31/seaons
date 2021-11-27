c60c7b605ed37079733ed200bc310a7a054a5ddd
Jonas Noori jonasgeneralassembly@gmail.com <jonasgeneralassembly@gmail.com>
2021-11-27 13:07:37 +0000

    1st class compoonent that exnted from React.Componenet

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
+
+//functional component
+//const App = () => {
+	//window.navigator.geolocation.getCurrentPosition(
+		//position => console.log(position),
+		//err => console.log(err)
+	//);
+
+	//return <div>Hi there!</div>;
+
+//};
+
+//class component with render method assigned
+// extneds is like inheritence in ruby where we inherit React.Component class
+class App extends React.Component {
+	render() {
+		window.navigator.geolocation.getCurrentPosition(
 		position => console.log(position),
 		err => console.log(err)
 	);
-
-	return <div>Hi there!</div>;
-
-};
+		return <div>Latitude:</div>;
+	}
+}
 
 ReactDOM.render( <App />, document.querySelector('#root'));
