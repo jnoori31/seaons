9128d6b364e4f3a9c05090a3d0a3ad7fade165ad
Jonas Noori jonasgeneralassembly@gmail.com <jonasgeneralassembly@gmail.com>
2021-12-18 20:17:21 +0000

    determined season function with ternary expressions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './seasonDisplay';
 
 
 //functional component
@@ -36,7 +37,8 @@ class App extends React.Component {
 			return <div>Error: {this.state.errorMessage}</div>
 		}
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>latitude: {this.state.lat}</div>
+			// return <div>latitude: {this.state.lat}</div>
+			return <SeasonDisplay lat={this.state.lat} />
 		}
 		return <div>Loading!</div>;
 	}
--- src/seasonDisplay.js
+++ src/seasonDisplay.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-const SeasonDisplay = () => {
-	return <div>Season Display</div>;
+
+const getSeason = (lat, month) => {
+	if (month > 2 && month < 9) {
+		return lat > 0 ? 'summer' : 'winter';
+	} else {
+		return lat > 0 ? 'winter' : 'summer';
+	}
+};
+
+
+const SeasonDisplay = (props) => {
+	const season = getSeason(props.lat, new Date().getMonth());
+	const text = season === 'winter' ? 'Its winter....Burr, it is nippy' : 'Lets chirps a beach'
+
+	return ( 
+		<div>
+			<h1>{text}</h1>
+		</div>
+	);
 };
 
 export default SeasonDisplay;
